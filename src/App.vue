6bce67e871168a0de2927a5118b9c00d76e0d900
Jens Zakarias Pedersen <jens7388@edu.campusvejle.dk>
2020-12-16 15:00:27 +0100

    did some styling

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,26 @@
 <template>
- <header>
-    <h3>Remember me</h3>
-    <button>+ Add</button>
-  </header>
-  <resources></resources>
+  <div>
+    <header class="flex justify-around">
+      <h3 class="text-gray-500 font-semibold">Remember me</h3>
+      <button>+ Add</button>
+    </header>
+    <div class="flex flex-col ">
+      <resources />
+    </div>
+      <button>Add resource</button>
+    
+  </div>
 </template>
 
 <script>
-import Resources from './components/Resources.vue'
+import Resources from "./components/Resources.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    Resources
-  }
-}
+    Resources,
+  },
+};
 </script>
 
 <style>
