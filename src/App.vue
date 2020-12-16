dd6a41923769bc84be9eed76ae0eb6b6033721cd
Jens Zakarias Pedersen <jens7388@edu.campusvejle.dk>
2020-12-16 11:29:38 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <resources></resources>
+</template>
+
+<script>
+import Resources from './components/Resources.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Resources
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/Resources.vue
@@ -0,0 +1,27 @@
+<template>
+    <section v-for="resource in storedResources" :key="resource.id">
+        <h3> {{ resource.header }} </h3>
+        <p>  {{ resource.desc }}</p>
+    </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      storedResources: [
+        {
+          id: "official-guide",
+          header: "Official guide",
+          desc: "The official Vue.js documentation.",
+        },
+        {
+          id: "google",
+          header: "Google",
+          desc: "Learn to google....",
+        },
+      ],
+    };
+  },
+};
+</script>
