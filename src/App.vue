5cf78a4ca0177ba4187634e00346fc10b9eebda2
Jens Zakarias Pedersen <jens7388@edu.campusvejle.dk>
2020-12-17 14:58:59 +0100

    final cleanup

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div class="max-w-2xl m-auto py-10">
     <header class="flex justify-between px-5">
       <h3 class="text-gray-400 mt-2 font-semibold">REMEMBER ME</h3>
-      <button class=" px-4 py-2 rounded-md bg-blue-100 text-blue-700 hover:bg-blue-800 hover:text-white">+ Add</button>
+      <button class="px-4 py-2 rounded-md bg-blue-100 text-blue-700 hover:bg-blue-800 hover:text-white">+ Add</button>
     </header>
     <div class="flex flex-col ">
       <resources />
